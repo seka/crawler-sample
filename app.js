3b263bb47811aae81b0284b05afc1b6811bc852f
seka <s.seka134704@gmail.com>
2015-10-01 02:01:47 +0900

    httpリクエストの送信

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+var http = require('http');
+
+var req = http.get('http://www.w3.org/TR/css-2010/', function(res) {
+  res.setEncoding('utf8');
+  res.on('data', function(str) {
+    console.log(str);
+  });
+});
+
+req.on('error', function(err) {
+  console.log("Error: " + err.message);
+});
 
